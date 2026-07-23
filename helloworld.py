bbc36259d3a6efe855b9812a17a17dbd12a29b39
Dominic Ofili <dominicofili@gmail.com>
2026-07-23 10:16:07 +0100

    added a new function which converts miles to kilometres. Tested it with an example

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-#basket total with discount program
+# Basket total with discount program
 
 def basket_total(prices, discount=0.0):
     subtotal = sum(prices)
@@ -6,10 +6,17 @@ def basket_total(prices, discount=0.0):
 
 print(basket_total([1.35, 2.20, 0.99], discount=0.1))  # Example usage with a 10% discount
 
-#tip splitter program
+# Tip splitter program
 
 def tip_splitter(bill, tip_percent=0.15, people=1):
     total = bill * (1 + tip_percent)
     return round(total / people, 2)
 
 print(tip_splitter(80, tip_percent=0.15, people=4))  # Example usage with a 15% tip and 4 people and total bill of 80
+
+# Example unit converter (from miles to kilometers)
+
+def miles_to_km(miles):
+    return round(miles * 1.60934, 2)
+
+print(miles_to_km(10))  # Example usage with 10 miles
